cb9962923acc0f2cf1be292c9de44edd2d1a2773
wiolettakurek <wioletta.kurek@azimo.com>
2020-02-27 21:14:53 +0100

    change i_matrix

--- project1/generation/generation.py
+++ project1/generation/generation.py
@@ -4,8 +4,7 @@ import sys
 
 
 def save_to_adjacency_matrix(graph_arr):
-    max_vertex = max(max(x) for x in graph_arr) + 1
-    adjacency_matrix = [[0] * max_vertex for i in range(max_vertex)]
+    adjacency_matrix = [[0] * get_max_vertex(graph_arr) for i in range(get_max_vertex(graph_arr))]
 
     for row in graph_arr:
         # row[0] - first vertex, row[1] - second vertex
@@ -17,23 +16,27 @@ def save_to_adjacency_matrix(graph_arr):
 
 
 def save_to_adjacency_list(graph_arr):
+    max_vertex = max(max(x) for x in graph_arr) + 1
     adjacency_list = []
 
-    index = 0;
-    adjacency_list.append([str(graph_arr[0][0]) + ":"])
-    for i in range(0, len(graph_arr)):
-        if i > 0 and graph_arr[i - 1][0] != graph_arr[i][0]:
-            index += 1
-            adjacency_list.append([str(graph_arr[i][0]) + ":"])
-        if (graph_arr[i][1] >= 0):
-            adjacency_list[index].append(graph_arr[i][1])
+    # index = 0;
+    # adjacency_list.append([str(graph_arr[0][0]) + ":"])
+    # for i in range(0, len(graph_arr)):
+    #     if i > 0 and graph_arr[i - 1][0] != graph_arr[i][0]:
+    #         index += 1
+    #         adjacency_list.append([str(graph_arr[i][0]) + ":"])
+    #     else:
+    #         found_before = -1
+    #         for j in range(0, graph_arr[i][0]):
+    #
+    #     if (graph_arr[i][1] >= 0):
+    #         adjacency_list[index].append(graph_arr[i][1])
 
     return adjacency_list
 
 
 def save_to_incidence_matrix(graph_arr):
-    max_vertex = max(max(x) for x in graph_arr) + 1 - get_amount_of_empty_vertex(graph_arr)
-    incidence_matrix = [[0] * (len(graph_arr) - get_amount_of_empty_vertex(graph_arr)) for i in range(max_vertex)]
+    incidence_matrix = [[0] * (len(graph_arr) - get_amount_of_empty_vertex(graph_arr)) for i in range(get_max_vertex(graph_arr))]
 
     for i in range(0, len(graph_arr) - get_amount_of_empty_vertex(graph_arr)):
         # row[0] - first vertex, row[1] - second vertex
@@ -45,3 +48,6 @@ def save_to_incidence_matrix(graph_arr):
 
 def get_amount_of_empty_vertex(graph_arr):
     return sum(x.count(-1) for x in graph_arr)
+
+def get_max_vertex(graph_arr):
+    return max(max(x) for x in graph_arr) + 1
