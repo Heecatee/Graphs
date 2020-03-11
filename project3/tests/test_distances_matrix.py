b5a812229c92a6b73a1269edad26e038ac2dc178
wiolettakurek <wioletta.kurek@azimo.com>
2020-03-11 20:25:29 +0100

    add distances matrix

--- project3/tests/test_distances_matrix.py
+++ project3/tests/test_distances_matrix.py
@@ -15,9 +15,7 @@ def main():
     graph_utils.print_2d_array("Initial data", g.random_connected_weighted_graph(1, 10))
     g.draw()
 
-    start_vertex = int(input("Starting vertex (0-" + str(len(g.graph_arr) - 1) + "): "))
-    dijkstra_result, distances = g.dijkstra_algorithm(start_vertex)
-    graph_utils.print_2d_array("\nDijkstra algorithm output", dijkstra_result)
+    graph_utils.print_2d_array("\nDijkstra algorithm output", g.get_distances_matrix())
 
 
 main()
