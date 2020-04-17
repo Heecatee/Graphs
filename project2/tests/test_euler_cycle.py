41e57b503f606152cc4a57748d7e0c4166f56ca2
wiolettakurek <wioletta.kurek@azimo.com>
2020-04-17 20:43:49 +0200

    update euler cycle test

--- project2/tests/test_euler_cycle.py
+++ project2/tests/test_euler_cycle.py
@@ -8,12 +8,10 @@ import graph.graph_utils as graph_utils
 
 def main():
     g = graph.create_from_file()
-    euler_cycle = g.euler_cycle()
-    print("Euler cycle", euler_cycle)
 
     random_g, random_c = graph().create_random_euler(8, 3, 1)
     random_g.draw()
-    print(random_c)
+    print("Euler cycle", random_c)
 
 
 main()
