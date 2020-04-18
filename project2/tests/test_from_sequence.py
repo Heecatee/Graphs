c4534404b1015bd23f27ed76e1237818bbc1e1ad
wiolettakurek <wioletta.kurek@azimo.com>
2020-04-18 16:21:11 +0200

    add comments to test from sequence

--- project2/tests/test_from_sequence.py
+++ project2/tests/test_from_sequence.py
@@ -3,8 +3,14 @@
 import sys
 sys.path.append('../..')
 from graph.graph import graph
+import graph.graph_utils as graph_utils
 
 graph1 = graph.create_from_sequence([4,2,2,3,2,1,4,2,2,2,2])
+graph_utils.print_2d_array("Ciag graficzny", graph1.get_adjacency_matrix())
 graph1.draw()
 
+print("Ciag niegraficzny:")
 graph_wrong = graph.create_from_sequence([4,2,2,3,2,1,4,2,2,2,1])
+
+print("Ciag niegraficzny:")
+graph_wrong2 = graph.create_from_sequence([4,-2,1,4,2,2,2,1])
