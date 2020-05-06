e34e045bd7de2ab318dd890332c2de8406f7ca86
ichtiostega <ichtiostega@hal-9000.kocierz.com>
2020-05-06 20:26:18 +0200

    Working version

--- project5/test1.py
+++ project5/test1.py
@@ -6,10 +6,6 @@ sys.path.append('../..')
 from graph.directed_graph import DirectedGraph
 from graph.flow_web import FlowWeb
 
-# graph = DirectedGraph()
-# graph.random(10, 0.5)
-# print(graph.graph_arr)
-# graph.draw()
-
 graph2 = FlowWeb(3)
-print(graph2.structure)
+print(graph2)
+graph2.draw()
